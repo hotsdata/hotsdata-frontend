b789a6e56ca022cbf466e4ab5ea01655d1c9d318
Jayson Bailey <avoidiam@gmail.com>
2017-02-23 15:40:42 -0700

    pages for register and signin

---
+++ src/components/SignIn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SignIn extends React.Component {
+
+  render() {
+    return (
+      <div>
+      Sign In
+      </div>
+    );
+  }
+
+}
+
+export default SignIn;
--- src/router.js
+++ src/router.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import Home from './components/Home';
+import Home     from './components/Home';
+import Register from './components/Register';
+import SignIn   from './components/SignIn';
 
 const Routes = () => {
   return (
     <Router history={hashHistory}>
-      <Route path="/" component={Home} />
+      <Route path="/" component={Home}>
+        <Route path="/register" component={Register} />
+        <Route path="/signin" component={SignIn} />
+      </Route>
     </Router>
   );
 };
