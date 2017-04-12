2b28d775435002924c81bfb293578ee884c909c7
Jayson Bailey <avoidiam@gmail.com>
2017-04-12 14:28:21 -0600

    fix server

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 8080;
 
-const indexPath = path.join(__dirname, '/src');
-const publicPath = express.static(path.join(__dirname, './dist'));
+// Server routes...
+app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
-app.use('/dist', publicPath);
-app.get('*', (_, res) => {
-  res.sendFile(indexPath);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
-app.listen(port);
-console.log("SERVER STARTED on PORT ", port);
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
