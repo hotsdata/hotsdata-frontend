404f1df891e3e483505bc7a3458c83ae6bddf81a
Jayson Bailey <avoidiam@gmail.com>
2017-04-26 14:15:42 -0600

    basic tabs working using react-tabs

--- src/containers/ReplaysShowPage.js
+++ src/containers/ReplaysShowPage.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Table } from 'semantic-ui-react';
+import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
 
 import { fetchCurrentReplay } from '../actions/replay_actions';
-import ReplayNav from '../components/replays/ReplayNav';
 import MatchStatsTable from '../components/replays/MatchStatsTable';
 import TalentsTable from '../components/replays/TalentsTable';
 import MatchPlayerPerformance from '../components/replays/MatchPlayerPerformance';
@@ -12,21 +11,11 @@ import MatchPlayerPerformance from '../components/replays/MatchPlayerPerformance
 class ReplaysShowPage extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      currentTab: 'stats'
-    }
-
-    this.changeTab = this.changeTab.bind(this);
   }
 
   componentWillMount() {
     let replayId = this.props.routeParams.replayId;
     this.props.fetchCurrentReplay(replayId)
-  }
-
-  changeTab(tabName) {
-    this.setState({ currentTab: tabName });
   }
 
   render() {
@@ -39,19 +28,25 @@ class ReplaysShowPage extends Component {
         <div className="replay-header">
           <h1>{this.props.currentReplay.mapName}</h1>
         </div>
-        <ReplayNav onItemClick={this.changeTab} />
-        <section className="stats-summary" style={{display: (this.state.currentTab === 'stats' ? 'inherit' : 'none')}}>
-          <h3>Match Stats</h3>
-          <MatchStatsTable />
-        </section>
-        <section className="talents-table" style={{display: (this.state.currentTab === 'talents' ? 'inherit' : 'none')}}>
-          <h3>Talents</h3>
-          <TalentsTable />
-        </section>
-        <section className="player-performance" style={{display: (this.state.currentTab === 'performance' ? 'inherit' : 'none')}}>
-          <h3>Player Performance</h3>
-          <MatchPlayerPerformance />
-        </section>
+        <Tabs>
+          <TabList>
+            <Tab>Stats</Tab>
+            <Tab>Talents</Tab>
+            <Tab>Performance</Tab>
+          </TabList>
+          <TabPanel className="stats-summary">
+            <h3>Match Stats</h3>
+            <MatchStatsTable />
+          </TabPanel>
+          <TabPanel className="talents-table">
+            <h3>Talents</h3>
+            <TalentsTable />
+          </TabPanel>
+          <TabPanel className="player-performance">
+            <h3>Player Performance</h3>
+            <MatchPlayerPerformance />
+          </TabPanel>
+        </Tabs>
       </div>
     )
   }
