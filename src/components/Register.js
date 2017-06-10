e93e41a6a3a23ffb02c26c72524d594669e40a23
Tobias Berg <tb@tobiasberg.net>
2017-06-10 13:23:18 +0800

    Change casing for user actions file name

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,7 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-import { registerUser } from '../actions/user_actions';
+import { registerUser } from '../actions/UserActions';
 import Auth from '../lib/Auth';
 
 import './SignIn.scss';
