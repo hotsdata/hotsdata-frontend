ecb541961f0c2414918c763464b991cc032917bb
Jayson Bailey <avoidiam@gmail.com>
2017-06-19 15:34:48 -0600

    WIP

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -3,36 +3,30 @@ import React, { Component } from 'react';
 class ProfilePage extends Component {
   constructor(props) {
     super(props)
-
-    this.state = {
-      accountId: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.setAccount = this.setAccount.bind(this);
-  }
-
-  componentWillMount() {
-    let storedAccountId = localStorage.getItem('accountId');
-    if (storedAccountId) {
-      this.setState({...this.state, accountId: storedAccountId});
-    }
-  }
-
-  handleChange(e) {
-    this.setState({...this.state, accountId: e.target.value});
-  }
-
-  setAccount() {
-    localStorage.setItem('accountId', this.state.accountId);
   }
 
   render() {
     return (
-      <div>
-        <h3>
-          Profile page is coming soon!  We plan on it being a dashboard with your hot and cold heroes, metric tracking, and various other trends.
-        </h3>
+      <div className="profile-page">
+        <div className="filters">
+        </div>
+        <div className="heroes-table">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Hero</th>
+                <th>Win Rate</th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
