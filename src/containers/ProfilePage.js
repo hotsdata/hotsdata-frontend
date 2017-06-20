fe05144a4a27f0c5f227716dc3a4b28ecc558d0a
Jayson Bailey <avoidiam@gmail.com>
2017-06-19 21:05:36 -0600

    reactable working

--- src/containers/ProfilePage.js
+++ src/containers/ProfilePage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Table, Thead, Tbody, Th, Tr, Td} from 'reactable';
 
 class ProfilePage extends Component {
   constructor(props) {
@@ -6,26 +7,49 @@ class ProfilePage extends Component {
   }
 
   render() {
+    let data = [
+      {hero: "Valla", winRate: 52.1},
+      {hero: "Arthas", winRate: 48.7},
+      {hero: "Kerrigan", winRate: 55.3},
+      {hero: "Tyrande", winRate: 52.7}
+    ];
+
+    let tableRows = data.map((d) => {
+      return (
+        <Tr key={d.hero}>
+          <Td column="hero">{d.hero}</Td>
+          <Td column="winRate">{d.winRate}</Td>
+        </Tr>
+      )
+    });
+
+    let sortable = [
+      {
+        column: 'hero',
+        sortFunction: (a, b) => {
+          return a[0].localeCompare(b[0]);
+        }
+      },
+      {
+        column: 'winRate',
+        sortFunction: (a, b) => {
+          return a < b;
+        }
+      }
+    ]
+
     return (
       <div className="profile-page">
         <div className="filters">
         </div>
         <div className="heroes-table">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>Hero</th>
-                <th>Win Rate</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
+          <Table className="table sortable striped" sortable={sortable}>
+            <Thead>
+              <Th column="hero">Hero</Th>
+              <Th column="winRate">Win Rate</Th>
+            </Thead>
+            {tableRows}
+          </Table>
         </div>
       </div>
     )
