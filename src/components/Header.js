362538f8201f27cbf482da9d491b68a9e11c455b
Jayson Bailey <avoidiam@gmail.com>
2017-07-16 10:02:49 -0600

    change top nav to put profile dropdown on left and not strect across screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,17 +6,17 @@ import ProfileDropdown from './ProfileDropdown';
 const Header = (props) => {
   return (
     <div className="site-header fixed">
+      <div className='nav-link'>
+        <ProfileDropdown />
+      </div>
       <div className='nav-link brand'>
-        <Link to="/">HotsData</Link>
+        <Link to="/">About</Link>
       </div>
       <div className='nav-link'>
         <Link to="/replays">Replays</Link>
       </div>
       <div className='nav-link'>
         <Link to="/upload">Upload</Link>
-      </div>
-      <div className='nav-link'>
-        <ProfileDropdown />
       </div>
     </div>
   );
