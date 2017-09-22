9342fc6cc4381596a2c91adaf9251e550faf420a
acernek <acernek@byrsp.com>
2017-09-21 20:14:45 -0700

    server (refactor): misc improvements

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const port = process.env.PORT || 3050;
 const app = express();
 
 // Server routes...
 app.get('/hello', (req, res) => res.send({ hi: 'there' }));
 
 if (process.env.NODE_ENV !== 'production') {
+  
   const webpackMiddleware = require('webpack-dev-middleware');
   const webpack = require('webpack');
   const webpackConfig = require('./webpack.config.js');
   app.use(webpackMiddleware(webpack(webpackConfig)));
+
 } else {
   app.use(express.static('dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
+  
+  app.route('*')
+    .get((req, res) => {
+      res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
+} 
 
-app.listen(process.env.PORT || 3050, () => console.log('Listening'));
+app.listen(port, () => console.log('Listening on port ${port}'));
